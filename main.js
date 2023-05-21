50005c830ccfaaf9ebe75ffe4bdc743088f8920d
Doruk <dorukyogscast@gmail.com>
2023-05-21 20:46:06 +0200

    UI Initilized

---
+++ main.js
@@ -0,0 +1,16 @@
+const ContainerContainer = { "thome": "head", "ttrades": "trades", "tsettings": "settings" }
+
+window.onload = () => {
+    Array.from(document.getElementsByClassName("tab")).forEach(tab =>
+        tab.addEventListener("click", tked => {
+            const tid = tab.getAttribute("id");
+            const VisibleContainer = document.querySelector(".container.visible")
+            const VisContName = document.querySelector(".container.visible").getAttribute("id");
+
+            if (VisContName && tid && ContainerContainer[tid] !== VisContName) {
+                VisibleContainer.classList.remove("visible");
+                document.getElementById(ContainerContainer[tid]).classList.add("visible");
+            }
+        })
+    )
+}
